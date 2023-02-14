0b873130d185dd53fd74d551172ce21b6c6b5bb0
ducduy192 <duy192203@gmail.com>
2023-02-14 16:36:45 +0700

    second commit

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,8 @@
-export const getHomePage = (req, res) => {
+import db from '../models/index';
+
+export const getHomePage = async (req, res) => {
+    const data = await db.User.findAll({ raw: true });
     console.log('>>> check data ');
+    console.log(data);
     return res.render('index.ejs');
 };
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import configViewEngine from './config/viewEngine';
 import initWebRoute from './route/web';
+import connectDB from './config/connectDB';
 require('dotenv').config();
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(bodyParser.json());
 
 configViewEngine(app);
 initWebRoute(app);
+
+connectDB();
 
 const port = process.env.PORT || 3000;
 
