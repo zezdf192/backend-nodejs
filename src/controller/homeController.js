6057ec94c14934cdce32a19f46d1a2f5af44f2e5
ducduy192 <duy192203@gmail.com>
2023-02-15 09:33:51 +0700

    migration and model

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -2,7 +2,5 @@ import db from '../models/index';
 
 export const getHomePage = async (req, res) => {
     const data = await db.User.findAll({ raw: true });
-    console.log('>>> check data ');
-    console.log(data);
-    return res.render('index.ejs');
+    return res.render('index.ejs', { data: JSON.stringify(data) });
 };
